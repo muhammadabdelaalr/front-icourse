f40a06f5309d4691e8a1fd5e2b9b61612f3f4e19
Adham Fouad <adhamfouad49@gmail.com>
2023-11-25 01:44:53 +0200

    now videos are dynamic

--- src/app/pages/course-videos/course-videos.component.ts
+++ src/app/pages/course-videos/course-videos.component.ts
@@ -3,14 +3,30 @@ import { Component, Injectable, OnInit } from '@angular/core';
 import { AuthService } from '../auth/login/auth.service';
 import { ActivatedRoute } from '@angular/router';
 
+export interface VideoDetail {
+  id: number;
+  videoName: string;
+  videoUrl: string;
+  duration: string;
+  linkName: string;
+  linkUrl: string;
+  fileName: string;
+  fileUrl: string;
+}
+
+export interface CourseData {
+  lessons_list: string;
+}
+
 @Injectable()
 export class DataService {
   constructor(private http: HttpClient) { }
 
   getData(id: String) {
-    return this.http.get(`http://localhost:3000/api.php/courses/${id}`);
+    return this.http.get<CourseData[]>(`http://localhost:3000/api.php/courses/${id}`);
   }
 }
+
 @Component({
   selector: 'app-course-videos',
   templateUrl: './course-videos.component.html',
@@ -19,46 +35,35 @@ export class DataService {
 })
 export class CourseVideosComponent implements OnInit {
 
-  videoDetails = [
-    {
-      id: 1,
-      videoName: 'video one',
-      videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-      duration: '1.5m',
-      linkName: 'link 1',
-      linkUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo',
-      fileName: 'file 1',
-      fileUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo'
-    },
-    {
-      id: 2,
-      videoName: 'video two',
-      videoUrl: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-      duration: '5m',
-      linkName: 'link 2',
-      linkUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo',
-      fileName: 'file 1',
-      fileUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo'
-    }
-  ]
+ 
 
-  curr_video_url: string = this.videoDetails[0].videoUrl;
-  curr_video_name: string = this.videoDetails[0].videoName;
-  constructor(public auth:AuthService, private dataService: DataService, private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-  }
-
-  setId(vid_id: any) {
-
-    for (let video of this.videoDetails) {
-      if (video.id === vid_id) {
-        this.curr_video_url = video.videoUrl;
-        this.curr_video_name = video.videoName;
-        break;
+    videoDetails: VideoDetail[] = [];
+    curr_video_url: string = '';
+    curr_video_name: string = '';
+  
+    constructor(public auth:AuthService, private dataService: DataService, private route: ActivatedRoute) {}
+  
+    ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get('id');
+      if(id != null){
+        this.dataService.getData(id).subscribe((data: CourseData[]) => {
+          this.videoDetails = JSON.parse(data[0].lessons_list) as VideoDetail[];
+          this.curr_video_url = this.videoDetails[0].videoUrl;
+          this.curr_video_name = this.videoDetails[0].videoName;
+        });
       }
+      
+    }
+  
+    setId(vid_id: any) {
+      for (let video of this.videoDetails) {
+        if (video.id === vid_id) {
+          this.curr_video_url = video.videoUrl;
+          this.curr_video_name = video.videoName;
+          break;
+        }
+      }
+      console.log(vid_id);
+      console.log(this.curr_video_name);
+    }
   }
-  console.log(vid_id);
-}
-}

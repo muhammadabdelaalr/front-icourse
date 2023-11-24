1346d4577629b1598e33fd4b9a0e1d1fc104bda8
Adham Fouad <adhamfouad49@gmail.com>
2023-11-25 00:53:25 +0200

    course video is working but in static mode only

--- src/app/pages/course-videos/course-videos.component.ts
+++ src/app/pages/course-videos/course-videos.component.ts
@@ -32,25 +32,33 @@ export class CourseVideosComponent implements OnInit {
     },
     {
       id: 2,
-      videoName: 'video one',
-      videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-      duration: '1.5m',
-      linkName: 'link 1',
+      videoName: 'video two',
+      videoUrl: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      duration: '5m',
+      linkName: 'link 2',
       linkUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo',
       fileName: 'file 1',
       fileUrl: 'https://www.youtube.com/watch?v=bNyUyrR0PHo'
     }
   ]
 
-  video: string = this.videoDetails[0].videoUrl;
-
+  curr_video_url: string = this.videoDetails[0].videoUrl;
+  curr_video_name: string = this.videoDetails[0].videoName;
   constructor(public auth:AuthService, private dataService: DataService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
   }
 
-  setId(id: any) {
-    console.log(id);
+  setId(vid_id: any) {
+
+    for (let video of this.videoDetails) {
+      if (video.id === vid_id) {
+        this.curr_video_url = video.videoUrl;
+        this.curr_video_name = video.videoName;
+        break;
+      }
   }
+  console.log(vid_id);
+}
 }
